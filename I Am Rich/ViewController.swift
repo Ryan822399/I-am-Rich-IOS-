11eab4c2a4c2aaa4289c7fd934b125ffbc60301e
Ryan Hennes <ryan.c.hennnes@gmail.com>
2020-05-24 01:26:50 -0700

    Fist app created

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ryan Hennes on 5/21/20.
+//  Copyright © 2020 Ryan Hennes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
